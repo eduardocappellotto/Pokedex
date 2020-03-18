7d76fb8af207b12a32d27737f6c0e776cf3a6403
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-18 04:11:31 -0300

    Update NavBar.js

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Branding = styled.a`
-  -moz-user-select: none;
-  -website-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  -o-user-select: none;
-`;
-
-const Logo = styled.img`
-  height: 30px;
-  width: 30px;
-  margin-right: 0.5em;
-`;
-
-const NavBarStyle = styled.nav``;
 
 export default class NavBar extends Component {
     render() {
